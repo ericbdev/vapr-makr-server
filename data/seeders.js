dd99831074dae74f336775a1730b73261aed42f6
Eric <ericbdev@gmail.com>
2017-12-24 11:08:56 -0500

    Recipe model and table
    - Add in sample recipe
    - Add in table for recipes
    - Add inn seeder for recipe

--- data/seeders.js
+++ data/seeders.js
@@ -1,5 +1,6 @@
 import flavors from '../seeds/flavors.json';
 import manufacturers from '../seeds/manufacturers.json';
+import recipes from '../seeds/recipes.json';
 
 export const seedFlavors = (Model) => {
   flavors.forEach((item) => {
@@ -16,6 +17,22 @@ export const seedManufacturers = (Model) => {
       manufacturerId: item.manufacturerId,
       shortName: item.shortName,
       longName: item.longName,
+    });
+  });
+};
+
+export const seedRecipes = (Model) => {
+  recipes.forEach((item) => {
+    Model.create({
+      name: item.name,
+      resultAmount: item.resultAmount, 
+      resultStrength: item.resultStrength,
+      resultVG: item.resultVG,
+      resultPG: item.resultPG,
+      nicStrength: item.nicStrength,
+      nicVG: item.nicVG,
+      nicPG: item.nicPG,
+      flavors: item.flavors,
     });
   });
 };
